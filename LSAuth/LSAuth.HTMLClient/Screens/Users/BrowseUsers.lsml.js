06f9ce91c761411543f44d521848dfb82ef38e8b
JoshuaABooker <JoshuaABooker@DESKTOP-R5ANG0N>
2017-09-12 08:53:35 -0400

    myGraph - test

---
+++ LSAuth/LSAuth.HTMLClient/Screens/Users/BrowseUsers.lsml.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.BrowseUsers.Invite_execute = function (screen) {
+    // Write code here.
+    myGraph.callGraphApi2();
+};
---
+++ LSAuth/LSAuth.HTMLClient/Scripts/msalconfig.js
@@ -0,0 +1,14 @@
+﻿var msalconfig = {
+    graphInvitationEndpoint: "https://graph.microsoft.com/beta/invitations",
+    graphGroupMembersEndpoint: "https://graph.microsoft.com/v1.0/groups/{groupId}/members/$ref",
+    graphAPIScopes: ["https://graph.microsoft.com/.default"],
+    authorityHostUrl: "https://login.microsoftonline.com",
+    common: "common/oauth2/v2.0",
+    tenant: "infoconsulting.onmicrosoft.com",
+    //authorityUrl: msalconfig.authorityHostUrl + '/' + msalconfig.common,
+    clientID: "39683401-a60c-4bc6-b744-6dbaedef498d",
+    secret: "",
+    redirectUri2: "https://localhost:44300/",
+    redirectUri: location.origin,
+    groupId: "c3346f2f-94d5-401a-afb1-9ad77616d79f"
+};
